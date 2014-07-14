a83cfd12fbf081a7a5a1d00b10e2a06126f2bf55
mlessman <matthew.lessman@gmail.com>
2014-07-13 23:11:17 -0400

    commit

--- plot2.R
+++ plot2.R
@@ -1,1 +1,7 @@
-hello
+data <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'),header=F, sep=';')
+names(data) <-names(read.table('household_power_consumption.txt', header=TRUE,sep=";",nrows=1))
+data$datetime <- strptime(paste(data$Date,data$Time), format = "%d/%m/%Y %H:%M:%S")
+data$Global_active_power <- as.numeric(data$Global_active_power)
+with(data, plot(datetime, Global_active_power, type="l", xlab="", ylab = "Global Active Power (kilowatts)"))
+dev.copy(png, file= "plot2.png")
+dev.off()
