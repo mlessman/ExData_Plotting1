a91561341d89f3b7cec05f3643afb069ff1843d7
mlessman <matthew.lessman@gmail.com>
2014-07-13 22:26:02 -0400

    commit

--- plot1.R
+++ plot1.R
@@ -1,1 +1,5 @@
-hello
+ data <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'),header=F, sep=';')
+ names(data) <-names(read.table('household_power_consumption.txt', header=TRUE,sep=";",nrows=1))
+ hist(data$Global_active_power, col = "red", ylab = "Frequency", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+ dev.copy(png, file= "plot1.png")
+ dev.off()
