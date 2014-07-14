b7f405ad27464f4127126c8991d3cc4462ddcc1a
mlessman <matthew.lessman@gmail.com>
2014-07-13 23:49:26 -0400

    commit

--- plot3.R
+++ plot3.R
@@ -1,1 +1,12 @@
-hello
+data <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'),header=F, sep=';')
+names(data) <-names(read.table('household_power_consumption.txt', header=TRUE,sep=";",nrows=1))
+data$datetime <- strptime(paste(data$Date,data$Time), format = "%d/%m/%Y %H:%M:%S")
+data$Sub_metering_1 <- as.numeric(data$Sub_metering_1)
+data$Sub_metering_2 <- as.numeric(data$Sub_metering_2)
+data$Sub_metering_3 <- as.numeric(data$Sub_metering_3)
+with(data, plot(datetime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering", col = "black"))
+lines (data$datetime, data$Sub_metering_2, type = "l", col = "red")
+points (data$datetime, data$Sub_metering_3, type = "l", col = "blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+dev.copy(png, file= "plot3.png", width = 480, height = 480)
+dev.off()
